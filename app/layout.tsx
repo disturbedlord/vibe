6e341fbb42a344a69af6b6b7b7ef5e781c9b7d7c
Avinash K <todisturbedlord@gmail.com>
2026-03-21 15:16:13 +0530

    01: Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.className} ${geistMono.className} min-h-full flex flex-col antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
