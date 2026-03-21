2a32261008ebaa594e0bcfd0c21056fbb9154142
Avinash K <todisturbedlord@gmail.com>
2026-03-21 19:55:07 +0530

    04: inngest background job

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-import { caller } from "@/trpc/server";
+"use client";
 
-const Page = async () => {
-  const data = await caller.createAI({ text: "hi" });
-  return <div className="font-bold">{JSON.stringify(data)}</div>;
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@base-ui/react";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    }),
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: "John" })}
+        className="font-bold text-white bg-black p-4 rounded-2xl"
+      >
+        Invoke Background Job
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
